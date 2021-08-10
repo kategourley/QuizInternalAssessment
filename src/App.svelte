9820676743d785b439c21bad6584c0e47ca0d142
kategourley <55566486+kategourley@users.noreply.github.com>
2021-08-10 00:22:39 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  let sharks = [{question: 'How many reproductive organs do male sharks have?', answer1: '3'}]
+  let big cats = [{question: 'If you throw a tiger in a lake will it drown?', answer1: 'no', answer2: 'yes'}]
+  let frogs = [{question: 'Which of the following is a toad?', answer1: 'dnsghsjdfhzk'}]
+</script>
