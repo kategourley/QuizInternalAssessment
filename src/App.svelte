c3ab12556b4ac2dd134ad2a9cced9005a2b74b28
kategourley <55566486+kategourley@users.noreply.github.com>
2021-09-07 01:38:08 +0000

    Finished basic game mechanics and added a finish screen, the age and name screen. Next Steps: add comments, and add the last 8 questions. Testing: if I input "Mako" instead of "mako" the answer is incorrect Solution: need to make it so the answers aren't case sensitive

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,147 @@
 <script>
 	let userInput = ''
-	let activeQ = 0
-  let questions = [{hint1:'Sophie has half as many apples as Sally.' , hint2:'Sarah has three times as many apples as Sohpie.', hint3: 'Sally has twice as many apples as Sarah.', query: 'How many apples does Sally have?', correct: '6',}]
+  let a = 0
+  let result = ''
+  let score = 0
+  let hideNext = true
+  let hideSubmit = false
+  let hideQuiz = true
+  let hideUserInfo = false
+  let hideFinish = true
+  let name = ''
+  let age = ''
+  let error = ''
 
-		function checkAnswers() {
-			
-			
-	if (userInput == questions[0].correct) {
-			
-			console.log('correct')
+  let questions = [{hint1:'https://cdn.the-scientist.com/assets/articleNo/68759/aImg/42171/tiger-shark-l.jpg' , hint2:'They are one of the largest shark species, growing up to 5.5m and 900kg.', hint3: 'They are named for the distinctive grey stripes/spots littering the sides of their bodies.', query: 'What species of shark is this?', correct: 'tiger',}, {hint1:'https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/03/04/12/mako-shark-.jpg', hint2:'They are the fastest sharks in the world with speeds of 96km/h, which allows them to jump out of the water to heights of up to 9m.', hint3: 'Their favourite meal is swordfish, despite them being such a hard prey to catch.', query: 'What species of shark is this?', correct: 'mako',},]
 
-		} else {
-			console.log('incorrect')
+	function checkAnswers() {
+		hideSubmit = true
+		if (userInput == questions[a].correct || userInput == [questions[a].correct + ' shark'] || userInput == [questions[a].correct + 'shark'] || userInput == [questions[a].correct + ' sharks']) {
+			result = 'correct'
+			score += 1
+    } else if (userInput == '') {
+result = 'Please input a valid answer.'
+hideSubmit = false
+score += 0
+    } else { 
+			result = 'incorrect'
+			score += 0
 		}
+if (a == 9) {
+  hideQuiz = true
+  hideFinish = false
+} else {
+  if (result == 'incorrect' || result == 'correct') {
+      hideNext = false
+      } else {
+      hideNext = true
+    }
+}
+    
+
 	}
-	function getRandom() {
-  	let number = Math.floor(Math.random() * 11);
-		return number
+	
+	function next() {
+			a += 1
+		result = ''
+		userInput = ''
+   hideNext = true
+    hideSubmit = false
 	}
+
+  function start() {
+    if (name == '') {
+
+      error = 'Please enter a valid name.'
+    } else if (age == '' || 0 > age || age > 100){
+      error = 'Please enter a valid age. Only numbers between 0 and 100 are accepted.'
+      } else {
+      hideQuiz = false
+      hideUserInfo = true
+    }
+
+   
+   
+  }
+
+   function playAgain() {
+      hideFinish = true
+      hideQuiz = true
+      hideUserInfo = false
+    }
 </script>
 
+<div class:hideQuiz>
+<img src={questions[a].hint1} alt='{questions[a].correct}'>
+<!-- <p>
+	{questions[a].hint1}
+</p> -->
+<p>
+	{questions[a].hint2}
+</p>
+<p>
+	{questions[a].hint3}
+</p>
+<p>
+	{questions[a].query}
+</p>
+<label>
+	<input bind:value={userInput}>
+</label>
+  
+<br><br>
 
+ <button on:click={checkAnswers} class:hideSubmit>Submit</button>
+<button on:click={next} class:hideNext>Next</button>
+<p>
+	{result}
+</p>
+<p>
+	Score = {score}
+</p>
 
-{#each questions as {hint1, hint2, hint3, userInput, query, correct}}
+</div>
+<div class:hideUserInfo>
+  <h1>Welcome!</h1>
+  <label>Name: 
+    <input bind:value={name}>
+    </label>
+    <label>
+      Age: 
+      <input type=number min=0 max=10 bind:value={age}>
+      </label>
+      
+    <button on:click={start}>Start</button>
+<p>{error}</p>
+  </div>
+  <div class:hideFinish>
+<p> Well done, {name}!</p>
+<p>You got {score}/10 correct!</p>
+<button on:click={playAgain}>Play Again</button>
+    </div>
+<style>
+  img {
+    width: auto;
+    height: 200px;
+  }
 
-  <p>{hint1}</p>
-  <p>{hint2}</p>
-  <p>{hint3}</p>
-  <p>{query}</p>
+  .hideNext {
+    display: none;
+  }
 
-{/each}
+  .hideSubmit {
+    display: none;
+  }
 
-  <label>
-    <input bind:value={userInput}>
-  </label>
-  <br><br>
+.hideQuiz {
+    display: none;
+  }
 
-  <button on:click={checkAnswers}>Submit</button>
+.hideUserInfo {
+    display: none;
+  }
 
-
+  .hideFinish {
+    display: none;
+  }
+</style>
